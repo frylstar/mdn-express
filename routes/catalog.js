6370120a945e60976cfb189971b21f5d36ca90c1
shizhongzheng <chengxiaszz@sina.com>
2019-05-20 00:29:53 +0800

    controllers

---
+++ routes/catalog.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// 导入控制器模块
+const book_controller = require('../controllers/bookController');
+const author_controller = require('../controllers/authorController');
+const genre_controller = require('../controllers/genreController');
+const book_instance_controller = require('../controllers/bookinstanceController');
+// 藏书路由
