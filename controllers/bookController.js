28b23068b2cccfb318b85c4c3ed8b30c39197d75
shizhongzheng <chengxiaszz@sina.com>
2019-05-22 23:56:33 +0800

    Add async, pug

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,33 @@
 const Book = require('../models/book');
+const Author = require('../models/author')
+const Genre = require('../models/genre')
+const BookInstance = require('../models/bookinstance')
 
-exports.index = (req, res) => {
-  res.send('未实现，站点首页');
+const async = require('async')
+
+exports.index = (req, res, next) => {
+  // async.parallel()方法传递一个对象，其中包含用于获取每个模型计数的函数
+  // 这些函数都是在同一时间开始的，全部完成时，最终回调将与结果参数中的计数（或错误）一起被调用
+  async.parallel({
+    book_count: function(callback) {
+      Book.count({}, callback)
+    },
+    book_instance_count: function(callback) {
+      BookInstance.count({}, callback)
+    },
+    book_instance_available_count: function(callback) {
+      BookInstance.count({status: 'Available'}, callback);
+    },
+    author_count: function(callback) {
+      Author.count({}, callback)
+    },
+    genre_count: function(callback) {
+      Genre.count({}, callback)
+    }
+  }, function(err, results) {
+    // 指定名为'index'的视图（模板），以及一个对象包含了要插入其中的数据
+    res.render('index', { title: 'Local Library Home', err: err, data: results })
+  })
 }
 
 exports.book_detail = (req, res) => {
